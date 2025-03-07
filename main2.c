2b489b751f0c5bda980cb4c94c896d7875c16852
EkaterinaSadykovaa <Sadikova.ekaterina05@gmail.com>
2025-03-07 14:03:36 +0300

    add

--- main2.c
+++ main2.c
@@ -11,6 +11,7 @@
 
 int main(int argc, char *argv[]) {
     int option;
+    int users_flag = 0, processes_flag = 0;//*
 
     struct option long_options[] = {
         {"users", no_argument, NULL, 'u'},
@@ -20,29 +21,47 @@ int main(int argc, char *argv[]) {
         {"errors", required_argument, NULL, 'e'},
         {NULL, 0, NULL, 0}
     };
-
-    while ((option = getopt_long(argc, argv, "uph:l:e:", long_options, NULL)) != -1) {
+    
+    
+    if (argc < 2) {
+        fprintf(stderr, "No arguments provided. Use -h or --help for help.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    int option_index = 0;//*
+    while ((option = getopt_long(argc, argv, "uph:l:e:", long_options, &option_index)) != -1) {
         switch (option) {
             case 'u':
-            	function_users();
-                break;
+            	users_flag = 1;
+            	//function_users();
+            	return 0;
+                //break;
             case 'p':
-            	function_processes();
-                break;
+            	processes_flag = 1;
+            	//function_processes();
+            	return 0;
+                //break;
             case 'h':
             	function_help();
                 return 0;
             case 'l':
                 function_log(optarg);
-               
-                break;
+                return 0;
+                //break;
             case 'e':
             	function_errors(optarg);
-                break;
+            	return 0;
+                //break;
             default:
                 function_help();
                 return 1;
         }
+    }
+    if (users_flag) {       //*
+        function_users();
+    }
+    if (processes_flag) {
+        function_processes();
     }
 
 
